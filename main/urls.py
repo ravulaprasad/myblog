266a98e2f067fe7a7df19bbd2617f659adf9f148
ravulaprasad <ravulasaisimhaprasad@gmail.com>
2021-07-22 20:10:25 +0530

    trying to link urls to main

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
 
-	path('',views.home,name='home')
+	path('',views.home,name='home'),
+	path('signin',views.signin,name='signin')
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-	return render(request,'home.html',{'name':'Navin'})
+	return render(request,'home.html')
 
-def add(request):
-	val1=int(request.POST['num1'])
-	val2=int(request.POST['num2'])
-	res= val1 + val2
-	return render(request,'result.html',{'result':res})
+def signin(request):
+	return render(request,'signin.html')
