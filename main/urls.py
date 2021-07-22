c55fadadf6b6a1500c09a4c7e7ea4e1519e3c963
ravulaprasad <ravulasaisimhaprasad@gmail.com>
2021-07-22 20:49:48 +0530

    unable to link urls

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns=[
 
-	path('',views.home,name='home'),
-	path('signin',views.signin,name='signin')
+	path('',views.index,name='index'),
+	path('Home/',views.Home,name='Home'),
+	path('MVT/',views.MVT,name='MVT'),
+	path('Signup/',views.Signup,name='Signup'),
+	path('Login/',views.Login,name='Login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, resolve_url
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-	return render(request,'home.html')
+def index(request):
+	return render(request,'index.html')
 
-def signin(request):
-	return render(request,'signin.html')
+def Home(request):
+	return render(request,'Home.html')
+
+def MVT(request):
+	return render(request,'MVT.html')
+
+def Signup(request):
+	return render(request,'Signup.html')
+
+def Login(request):
+	return render(request,'Login.html')
